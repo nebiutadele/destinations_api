bae86163327e5d38cf1be4478b67ac3498560d2e
nebiutadele <nebiutadele@gmail.com>
2022-02-10 16:54:44 -0600

    set up dynamic port

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const db = require("./db")
 // Import an instance of express function
 const express = require("express")
+
+const PORT = process.env.PORT || 3000;
+
 //Set up instance of a server
 const app = express()
-//Set local port
-app.listen(3000)
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT: ${PORT}`)
+})
+
 //Create a response for when server is called
 app.get("/", (req, res) => {
     res.send(db)
