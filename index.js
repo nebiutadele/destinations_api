42e70b418b34ca7c5d6d0c1c19181bd7e8b9bf53
nebiutadele <nebiutadele@gmail.com>
2022-02-10 17:27:39 -0600

    cors package

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const db = require("./db")
+const cors = require("cors")
 // Import an instance of express function
 const express = require("express")
+
+
 
 // Make port equal to enviroment or 3000
 const PORT = process.env.PORT || 3000;
 
 //Set up instance of a server
 const app = express()
+
+app.use(cors())
 
 // Make the server listen to the port
 app.listen(PORT, () => {
